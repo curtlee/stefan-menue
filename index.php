1db9c8daf6f5e3df77100fbb296966181aa6475d
104447 <u39098550@false.(none)>
2012-10-26 02:06:10 +0200

    content and some style and logic changes

--- index.php
+++ index.php
@@ -9,27 +9,36 @@ $trees = simplexml_load_file("content/pagesConfig.xml");
 
 	// query XML via XPath to find all topMenu pages
 $topMenuPages = $trees->xpath("/trees/tree[@title='topMenu']/page");
-$topMenu = generateMenu($topMenuPages);
 $leftMenuPages = $trees->xpath("/trees/tree[@title='leftMenu']/page");
-$leftMenu = generateMenu($leftMenuPages);
 	// check if page exists in xml
-$requestedPageInXML = $trees->xpath("/trees/tree/page[@src='$requestedFilename']");
+if ( substr($requestedFilename, -5, 5) == ".html" ) {
+	$requestedPageInXML = $trees->xpath("/trees/tree/page[@src='$requestedFilename']");
+} else {
+	$requestedPageInXML = $trees->xpath("/trees/tree/page[@default='1']");
+	$requestedFilename = (string)$requestedPageInXML[0]['src'];
+}
 if ( count($requestedPageInXML) && file_exists('content/'.$requestedFilename) ) {
 	$content = file_get_contents('content/'.$requestedFilename);
 } else { // page not found
 	$content = file_get_contents('content/404.html');
 }
 
+$topMenu = generateMenu($topMenuPages);
+$leftMenu = generateMenu($leftMenuPages);
+$keyvisualStyle = isset($requestedPageInXML) ? "background: url('" . (string) $requestedPageInXML[0]['keyvisual'] . "');" : '';
+$backgroundStyle = isset($requestedPageInXML) ? "background-image: url('" . (string) $requestedPageInXML[0]['background'] . "');" : '';
+
 $template = str_replace('###TOP_MENU###', $topMenu, $template);
 $template = str_replace('###LEFT_MENU###', $leftMenu, $template);
 $template = str_replace('###CONTENT###', $content, $template);
+$template = str_replace('###KEYVISUAL_STYLE###', $keyvisualStyle, $template);
+$template = str_replace('###BACKGROUND_STYLE###', $backgroundStyle, $template);
 
 echo $template;
 
 
 function generateMenu($pages) {
 	global $requestedFilename;
-	
 	$menu = "<ul>\n";
 	foreach( $pages as $page ) {
 		if ( (string) $page['type'] == 'separator' ) {
